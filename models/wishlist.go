f5f9bb5cc2372e5c413f1785ad67127dafe990b7
Paul Schifferer <paul@schifferers.net>
2026-08-28 14:24:27 -0700

    feat(wishlist): add Name field for multi-wishlist support

--- models/wishlist.go
+++ models/wishlist.go
@@ -11,6 +11,7 @@ import (
 type Wishlist struct {
 	ID         string          `bson:"_id" json:"id" jsonapi:"primary,wishlist"`
 	UserID     string          `bson:"user_id" json:"user_id" jsonapi:"relation,user"`
+	Name       string          `bson:"name" json:"name" jsonapi:"attr,name"`
 	Visibility Visibility      `bson:"visibility" json:"visibility" jsonapi:"attr,visibility"`
 	Entries    []WishlistEntry `bson:"entries" json:"entries" jsonapi:"attr,entries"`
 	modelcore.Auditable
@@ -24,10 +25,11 @@ type WishlistEntry struct {
 
 // NewWishlist creates a wishlist defaulting to private visibility, per the
 // "new wishlists default to private" requirement.
-func NewWishlist(id, userID string) Wishlist {
+func NewWishlist(id, userID, name string) Wishlist {
 	return Wishlist{
 		ID:         id,
 		UserID:     userID,
+		Name:       name,
 		Visibility: VisibilityPrivate,
 	}
 }
